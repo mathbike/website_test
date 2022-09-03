ae27b91d2e8dc67e55180df5f787fa9575b5cb3b
mathbike <mathbike8@gmail.com>
2022-09-03 07:58:34 -0400

    d

---
+++ scripts.js
@@ -0,0 +1,25 @@
+///// COLOR THEME /////
+
+// Immediately invoked function to set the theme on initial load
+(function () {
+    if (localStorage.getItem('theme') === 'theme-dark') {
+        setTheme('theme-dark');
+        document.getElementById('slider').checked = false;
+    } else {
+        setTheme('theme-light');
+      document.getElementById('slider').checked = true;
+    }
+})()
+// function to set the theme and save selection to local storage
+function setTheme(themeName) {
+    localStorage.setItem('theme', themeName);
+    document.documentElement.className = themeName;
+}
+// function to toggle between themes
+function toggleTheme() {
+    if (localStorage.getItem('theme') === 'theme-dark') {
+        setTheme('theme-light');
+    } else {
+        setTheme('theme-dark');
+    }
+}
