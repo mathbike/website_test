78c1f6cab21e143d2be034e2ce38e87ad8d622af
mathbike <mathbike8@gmail.com>
2022-09-03 16:43:05 -0400

    d

---
+++ sc.js
@@ -0,0 +1,7 @@
+// highlight selected page
+function addSelected() {
+    $(".icons-links").click(function(event){
+        /* $(".icons-box").removeClass("icons-box"); */
+        $(".icons-box").addClass("selected");
+    })
+}
--- scripts.js
+++ scripts.js
@@ -33,10 +33,11 @@ function loadLogin() {
     $("#main").load('login.html')
 }
 
-// highlight selected page
+
 function addSelected() {
     $(".icons-links").click(function(event){
-        /* $(".icons-box").removeClass("icons-box"); */
-        $(".icons-box").addClass("selected");
-    })
+        $(".icons-box").removeClass("selected");
+        $(this.parentNode).addClass("selected");
+        return false;
+    });
 }
